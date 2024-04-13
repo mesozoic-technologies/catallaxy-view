791c5a378a63ceb9cb7e5d2e5d30f4104a6223d2
commercium-sys <system@commercium.systems>
2024-04-13 17:56:34 -0400

    Updates

--- main.unmin.js
+++ main.unmin.js
@@ -17,17 +17,17 @@
 
 
 
-const FAUCET_WETH="0x05A5e11F6cb8b0BE8898A7ae21407f8D6e7f6fAE"
-const CATALLAXY_SOVEREIGN="0x9b3468120872287868f1Fadd55ea369624EbdfE9"
-const CTLXY_USD="0xee5E23Fb9364CeA4620FD8121fC213A54f97ad84"
-const CTLXY_USD_INTEREST_RATE="0xC826d2099967Cd9e15f5d3F5825ca1C3cE8216d7"
-const CTLXY_ETH_INTEREST_RATE="0x713af1bBa747796B18eC20a42D28208E14da406C"
-const CTLXY_CTLXY_USD_INTEREST_RATE="0xeDaF1e44de13eff2D4830F75dEeC10a1C0021e2C"
-const CATALLAXY_COMPTROLLER="0x79549588eF8a67e84c339062AF74D1B4BB5572e5"
-const CTLXY_CTLXY_USD="0x8Bfe66EA5361d9357255e52219b626181B2A4622"
-const CTLXY_ETH="0xdCaBB4336Fe4BfC97754E79082927dE5B53C008A"
-const CTLXY_USD_FACILITY="0x993C7218C535589f5683D03Aa3cC485873B108d4"
-const CATALLAXY_FEED="0x16C06d4D1e63f070FCD786805AF6F7C2dd729aDC"
+const FAUCET_WETH="0x832c6D83Fc43466Ed8f0902F996A23E901015c5f"
+const CATALLAXY_SOVEREIGN="0xa83fDBFaeeAcE5140EFb89561048E5eD4DA3b0c3"
+const CTLXY_USD="0x132c0998190368ef6C1e22429EBAf65ce5B514B4"
+const CTLXY_USD_INTEREST_RATE="0x5B482e9d46912a831d92C1Ad5D34eEA6F56aa894"
+const CTLXY_ETH_INTEREST_RATE="0x7B69767eE72093023A76Faed86000376b1c201F2"
+const CTLXY_CTLXY_USD_INTEREST_RATE="0x9B00F11e31643D7524D52862C245154E8087389C"
+const CATALLAXY_COMPTROLLER="0xE79e490f9EbCD55D819DcE4d058e72db411d3842"
+const CTLXY_CTLXY_USD="0x558Df8E220348953343bABEba84Fb2580956d210"
+const CTLXY_ETH="0xe8F8c822c0Ea2b7032b313DBD948c2A06aD8b74e"
+const CTLXY_USD_FACILITY="0xfB0caC901E8c50eAE8f88DB251c974932898c836"
+const CATALLAXY_FEED="0x2dA256ED051EbcB0Da276a3eB3DAb16830B2EbE7"
 
 const erc20 = [
     "function allowance(address,address) view returns (uint)",
@@ -89,6 +89,7 @@ const ctlxyTokenAbi = (0,viem__WEBPACK_IMPORTED_MODULE_0__/* .parseAbi */ .U)(er
     "function borrowBalanceCurrent(address)",
     "function borrowBalanceStored(address)",
     "function borrowRatePerBlock()",
+    "function mint(address,uint)",
     "function cash()",
     "function comptroller()",
     "function exchangeRateCurrent()",
@@ -99,7 +100,7 @@ const ctlxyTokenAbi = (0,viem__WEBPACK_IMPORTED_MODULE_0__/* .parseAbi */ .U)(er
     "function totalBorrowsCurrent()",
 ]))
 
-const ctlxyUsdAbi = (0,viem__WEBPACK_IMPORTED_MODULE_0__/* .parseAbi */ .U)(erc20.concat([]))
+const ctlxyUsdAbi = (0,viem__WEBPACK_IMPORTED_MODULE_0__/* .parseAbi */ .U)(erc20)
 
 const jumpRateModelAbi = (0,viem__WEBPACK_IMPORTED_MODULE_0__/* .parseAbi */ .U)([
     "function baseRatePerBlock()",
@@ -118,7 +119,10 @@ const ctlxyUsdFacilityAbi = (0,viem__WEBPACK_IMPORTED_MODULE_0__/* .parseAbi */ 
 
 ])
 
+const MAXUINT  = BigInt(2)**BigInt(256) - BigInt(1);
+
 const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 const ERR_ACCT = '0x' + '1'.repeat(40);
 
 let account, transport, publicClient, walletClient
@@ -154,10 +158,83 @@ const drip = async () => {
     }
 }
 
-const updateWeth = async () => {
+const approve = async () => {
+    try {
 
+        const [a,b] = 
+            $('input[name="supply"]:checked').value == 'ETH' ? [ weth, ctlxyEth ] :
+            $('input[name="supply"]:checked').value == 'ctlxyUSD' ? [ ctlxyUsd, ctlxyCtlxyUsd ] :
+            null;
+
+        const { request } = await a.simulate.approve([b.address, MAXUINT])
+        const hash = walletClient.writeContract(request)
+        await publicClient.waitForTransactionReceipt({hash})
+
+    } catch (e) {
+        console.log("err", e)
+    }
+}
+
+const supply = async () => {
+
+    try {
+
+        const market = 
+            $('input[name="supply"]:checked').value == 'ETH' ? ctlxyEth :
+            $('input[name="supply"]:checked').value == 'ctlxyUSD' ? ctlxyCtlxyUsd :
+            null;
+        
+        const amount = $('#supplyAmount').value
+        const { request } = await market.write.mint([account, amount])
+        // const { request } = await market.simulate.mint([account, amount])
+        const hash = walletClient.writeContract(request)
+        await publicClient.waitForTransactionReceipt({hash})
+
+    } catch (e) {
+        console.log("err", e)
+    }
+
+}
+
+const checkApprove = async () => {
+
+    console.log("?")
+
+    const contract = 
+        $('input[name="supply"]:checked').value == 'ETH' ? weth :
+        $('input[name="supply"]:checked').value == 'ctlxyUSD' ? ctlxyUsd :
+        null;
+
+    const approved = await contract.read.allowance([account, CTLXY_ETH])
+
+    $('#btnApprove').style.display = approved == 0 ? '' : 'none';
+    $('#btnSupply').style.display = approved == 0 ? 'none' : '';
+
+}
+
+const updateWeth = async () => {
     const wethBal = await weth.read.balanceOf([account])
     $('#wethBal').textContent = wethBal
+}
+
+const updateCtlxyEth = async () => {
+
+    const ctlxyEthBal = await ctlxyEth.read.balanceOf([account])
+    $('#ctlxyEthBal').textContent = ctlxyEthBal
+
+}
+
+const updateCtlxyUsd = async () => {
+
+    const ctlxyUsdBal = await ctlxyUsd.read.balanceOf([account])
+    $('#ctlxyUsdBal').textContent = ctlxyUsdBal
+
+}
+
+const updateCtlxyCtlxyUsd = async () => {
+
+    const ctlxyCtlxyUsdBal = await ctlxyCtlxyUsd.read.balanceOf([account])
+    $('#ctlxyCtlxyUsdBal').textContent = ctlxyCtlxyUsdBal
 
 }
 
@@ -187,19 +264,27 @@ window.onload = async () => {
 
     weth = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: FAUCET_WETH, abi: wethAbi, client: _client })
     ctlxyEth = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CTLXY_ETH, abi: ctlxyTokenAbi, client: _client })
-    ctlxyUsd = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ addresss: CTLXY_USD, abi: ctlxyUsdAbi, client: _client })
-    ctlxyCtlxyUsd = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ addresss: CTLXY_CTLXY_USD, abi: ctlxyTokenAbi, client: _client })
+    ctlxyUsd = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CTLXY_USD, abi: ctlxyUsdAbi, client: _client })
+    ctlxyCtlxyUsd = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CTLXY_CTLXY_USD, abi: ctlxyTokenAbi, client: _client })
     comptroller = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CATALLAXY_COMPTROLLER, abi: comptrollerAbi, cleint: _client })
     feed = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CATALLAXY_FEED, abi: feedAbi, client: _client })
     ctlxyEthInterestRate = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CTLXY_ETH_INTEREST_RATE, abi: jumpRateModelAbi, client: _client })
     ctlxyUsdInterestRate = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CTLXY_USD_INTEREST_RATE, abi: jumpRateModelAbi, client: _client })
     ctlxyCtlxyUsdInterestRate = (0,viem__WEBPACK_IMPORTED_MODULE_6__/* .getContract */ .PF)({ address: CTLXY_CTLXY_USD_INTEREST_RATE, abi: jumpRateModelAbi, client: _client })
 
-    $("#btnDrip").addEventListener('click', async () => {
-        await drip()
-    })
+    $("#btnDrip").addEventListener('click', async () => await drip())
+    $("#btnSupply").addEventListener('click', async () => await supply())
+    $("#btnApprove").addEventListener('click', async () => await approve())
+
+    $$('input[name="supply"]').forEach(input => 
+        input.addEventListener('click', async () => await checkApprove())
+    )
 
     await updateWeth()
+    await updateCtlxyEth()
+    await updateCtlxyUsd()
+    await updateCtlxyCtlxyUsd()
+    await checkApprove()
 
 }
 
